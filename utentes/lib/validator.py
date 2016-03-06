22280934b135c3fd54b53403c3f66749a0cda2d4
Francisco Puga <fpuga@icarto.es>
2016-03-06 18:52:55 +0100

    Adds an add_rule method to the validator

--- utentes/lib/validator.py
+++ utentes/lib/validator.py
@@ -24,10 +24,20 @@ class Validator():
         for definition in self.schema:
             for rulename in definition['rules']:
                 rule = self.get_rule(rulename)
-                if rule.fails(model[definition['fieldname']]):
-                    self.messages.append(definition['message'])
+                print rule
+                if isinstance(rule, dict):
+                    print model
+                    model[definition['fieldname']]
+                    if rule['fails'](model[definition['fieldname']]):
+                        self.messages.append(definition['message'])
+                else:
+                    if rule.fails(model[definition['fieldname']]):
+                        self.messages.append(definition['message'])
 
         return self.messages
 
     def get_rule(self, rulename):
         return self.rules[rulename]
+
+    def add_rule(self, rulename, rule_def):
+        self.rules[rulename] = rule_def
