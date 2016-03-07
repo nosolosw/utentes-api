819640d4343476d7681f817100097068968e6c1f
Francisco Puga <fpuga@icarto.es>
2016-03-07 17:34:23 +0100

    Validation schema for the entities includes dates and numbers

--- utentes/models/exploracao_schema.py
+++ utentes/models/exploracao_schema.py
@@ -13,6 +13,10 @@ EXPLORACAO_SCHEMA = [{
   'message':   'Nome da exploracão non pode estar vacío',
   'rules':     ['NOT_NULL']
 }, {
+  'fieldname': 'd_soli',
+  'message':   'Data solicitude não tem o formato correcto',
+  'rules':     ['IS_DATE']
+}, {
   'fieldname': 'loc_provin',
   'message':   'A provincia da exploracão non pode estar vacío',
   'rules':     ['NOT_NULL']
@@ -24,6 +28,26 @@ EXPLORACAO_SCHEMA = [{
   'fieldname': 'loc_posto',
   'message':   'O posto da exploracão non pode estar vacío',
   'rules':     ['NOT_NULL']
+}, {
+  'fieldname': 'c_soli',
+  'message':   'Consumo solicitado não tem o formato correcto',
+  'rules':     ['IS_NUMERIC']
+}, {
+  'fieldname': 'c_licencia',
+  'message':   'Consumo licencia não tem o formato correcto',
+  'rules':     ['IS_NUMERIC']
+}, {
+  'fieldname': 'c_real',
+  'message':   'Consumo real não tem o formato correcto',
+  'rules':     ['IS_NUMERIC']
+}, {
+  'fieldname': 'c_estimado',
+  'message':   'Consumo estimado não tem o formato correcto',
+  'rules':     ['IS_NUMERIC']
+}, {
+  'fieldname': 'area',
+  'message':   'Área não tem o formato correcto',
+  'rules':     ['IS_NUMERIC']
 }, {
   'fieldname': 'utente',
   'message':   'A exploracão ten que ter asignado un utente',
