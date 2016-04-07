a50669837aee3c21359749aff33944f30509fb28
Andrés Maneiro <amaneiro@icarto.es>
2016-04-07 13:46:46 +0200

    fix error on msg key

--- utentes/api/error_msgs.py
+++ utentes/api/error_msgs.py
@@ -2,10 +2,10 @@
 
 # TODO translate
 error_msgs = {
-    'no_gid':         'Nao existe exploracao co gid indicado',
+    'no_gid': 'Nao existe exploracao co gid indicado',
     'gid_obligatory': 'O gid é un campo necesario',
     'body_not_valid': 'Os dados recibidos nao som un JSON válido',
     'exploracao_already_exists': 'A exploracao ja existe',
-    'exploracao_already_exists': 'O utente ja existe',
+    'utente_already_exists': 'O utente ja existe',
     'no_cultivo_gid': 'Nao existe cultivo co gid indicado',
 }
